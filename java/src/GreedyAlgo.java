8812ed4649ec04212a6eeac2b01498c2b6650d27
lbhsos <lbhsos29@naver.com>
2019-03-06 20:06:17 +0900

    Greedy beckjoon_2875 edit

--- java/src/GreedyAlgo.java
+++ java/src/GreedyAlgo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Greedy_2875 {
+public class GreedyAlgo {
     static int count=0;
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
