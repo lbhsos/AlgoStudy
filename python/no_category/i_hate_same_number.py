5109282bcc537fe4ddec14cf2c2156ffda123ab7
lbhsos <lbhsos29@naver.com>
2019-08-10 10:00:00 +0900

    clean project. Evidence of your effort. I love you

--- python/no_category/i_hate_same_number.py
+++
@@ -1,8 +0,0 @@
-def solution(arr):
-    last = arr[0]
-    answer = [last]
-    for item in arr:
-        if item != last : 
-            answer.append(item)
-            last = item
-    return answer
