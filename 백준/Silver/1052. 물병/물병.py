637d122acc58a85757ffe9b4865565c9ebaa5a45
Bohyun Lee <bohyunlee.dev@gmail.com>
2022-07-05 00:27:03 +0900

    [Silver I] Title: 물병, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.py
+++ 백준/Silver/1052. 물병/물병.py
@@ -1,27 +1,7 @@
-N, K = map(int, input().split(" "))
-i = 0
-ans = 0
-def get_binary_cnt(num):
-    global K
-    # print("---")
-    cnt = 0
-    while True:
-        div, mod = divmod(num, 2)
-        # print(div, mod)
-        if mod == 1:
-            cnt +=1
-
-        if div == 0:
-            break
-        num = div
-        # print(num, cnt)
-    if cnt <= K:
-        return True
-    return False
-
-while True:
-    if get_binary_cnt(N):
-        break
-    N += 1
-    ans += 1
-print(ans)
+N, K =map(int, input().split(" "))
+cnt = 0
+while bin(N).count('1') > K:
+    a = 2**(bin(N)[::-1].index('1'))
+    cnt += a
+    N += a
+print(cnt)
