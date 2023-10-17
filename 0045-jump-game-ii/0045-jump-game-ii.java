b6783a2869c074043b9dc8b56becc6762bf01c19
lbhsos <bohyunlee.dev@gmail.com>
2023-10-17 22:49:34 +0900

    jump-game-ii

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+      int count = 0;
+      int right = 0;
+      int farthest = 0;
+
+      for(int i = 0; i < nums.length - 1; i ++) {
+          farthest = Math.max(farthest, i + nums[i]);
+          if(i == right) {
+              right = farthest;
+              count++;
+          }
+      }  
+      return count;
+    }
+}
